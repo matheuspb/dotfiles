249447d85ee2627ebc513209b2b7d69ba03bb2a2
Matheus Bittencourt <bittencourt.matheus@gmail.com>
2016-11-29 18:30:54 -0200

    fish configs

---
+++ config.fish
@@ -0,0 +1,15 @@
+alias ll "ls -l"
+alias la "ls -A"
+alias lla "ls -lA"
+
+alias mv "mv -i"
+alias cp "cp -i"
+alias xflux "xflux -l -27.6 -g -48.5"
+
+alias bye "sudo shutdown 0"
+alias usage "du -bsh"
+alias mount-data "sudo mount -t auto -v /dev/sda6 /media/matheus/Data/"
+
+function cs
+	cd $argv; and ls
+end
