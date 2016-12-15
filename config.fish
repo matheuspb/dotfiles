8d281a6fc0e7881279034dd4011580a17e205157
Matheus Bittencourt <bittencourt.matheus@gmail.com>
2016-12-15 17:15:06 -0200

    urxvt configs and other changes

--- config.fish
+++ config.fish
@@ -6,7 +6,7 @@ alias mv "mv -i"
 alias cp "cp -i"
 alias xflux "xflux -l -27.6 -g -48.5"
 
-alias bye "sudo shutdown 0"
+alias bye "shutdown 0"
 alias usage "du -bsh"
 alias mount-data "sudo mount -t auto -v /dev/sda6 /media/matheus/Data/"
 
