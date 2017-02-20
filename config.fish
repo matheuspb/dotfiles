430d0922555bb4ca553929624cd027874342b3f7
Matheus Bittencourt <bittencourt.matheus@gmail.com>
2017-02-20 12:48:21 -0300

    Add alias

--- config.fish
+++ config.fish
@@ -10,6 +10,7 @@ alias rm "rm -Iv --one-file-system"
 
 alias usage "du -bsh"
 alias mount-data "sudo mount -t auto -v /dev/sda6 /media/matheus/Data/"
+alias server "python3 -m http.server &"
 
 # sudo !!
 function fuck
