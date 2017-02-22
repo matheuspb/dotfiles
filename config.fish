f7ae60fed962115e3a3642ef66b6258141a575f1
Matheus Bittencourt <bittencourt.matheus@gmail.com>
2017-02-21 22:51:44 -0300

    Added some enviroment variables and function 'dig'

--- config.fish
+++ config.fish
@@ -1,16 +1,22 @@
 # Put this file in ~/.config/fish/
 
-alias ll "ls -l"
-alias la "ls -A"
+export CC=clang
+export CXX=clang++
+export EDITOR=vim
+
+alias ll  "ls -l"
+alias la  "ls -A"
 alias lla "ls -lA"
+alias lsd "ls -l | grep '^d'"
 
 alias mv "mv -iv"
 alias cp "cp -iv"
 alias rm "rm -Iv --one-file-system"
 
+alias vim "vim -p"
+
 alias usage "du -bsh"
 alias mount-data "sudo mount -t auto -v /dev/sda6 /media/matheus/Data/"
-alias server "python3 -m http.server;"
 
 # sudo !!
 function fuck
@@ -19,4 +25,14 @@ end
 
 function cs
 	cd $argv; and ls
+end
+
+function dig
+	if test (count $argv) -eq 1
+		find . -iname "*$argv[1]*"
+	else if test (count $argv) -eq 2
+		find $argv[1] -iname "*$argv[2]*"
+	else
+		echo "No arguments"
+	end
 end
